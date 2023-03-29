f3f4901f348fd19a11ca7017f52612c21a241b9f
Nosipho <manyasenosipho@gmail.com>
2023-03-29 11:25:57 +0200

    create feed component and its header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Feed from '../../components/Feed'
 import Sidebar from '../../components/Sidebar'
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
         <Sidebar />
 
         {/* Feed section */}
+        <Feed />
 
         {/* Widgets */}
 
