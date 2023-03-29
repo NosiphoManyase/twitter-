e0235268d8e822540b5c1fa7195ee8192b74f68d
Nosipho <manyasenosipho@gmail.com>
2023-03-29 10:54:37 +0200

    create sidebar component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Sidebar from '../../components/Sidebar'
 
 export default function Home() {
   return (
@@ -9,7 +10,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello world</h1>
+      
+      <main className='flex min-h-screen max-w-7xl mx-auto'>
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Feed section */}
+
+        {/* Widgets */}
+
+        {/* Modal */}
+      </main>
     </>
   )
 }
