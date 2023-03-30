f284f591a39999007dcbafa8c861c24e750a9b05
Nosipho <manyasenosipho@gmail.com>
2023-03-30 05:45:28 +0200

    create news section of widget component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import Feed from '../../components/Feed'
 import Sidebar from '../../components/Sidebar'
 import Widgets from '../../components/Widgets'
 
-export default function Home() {
+export default function Home({ newsResults}) {
+  console.log(newsResults)
+
   return (
     <>
       <Head>
@@ -13,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      <main className='flex min-h-screen max-w-7xl mx-auto'>
+      <main className='flex min-h-screen mx-auto'>
         {/* Sidebar */}
         <Sidebar />
 
@@ -21,10 +23,24 @@ export default function Home() {
         <Feed />
 
         {/* Widgets */}
-        <Widgets />
+        <Widgets newsResults={newsResults.articles} />
 
         {/* Modal */}
       </main>
     </>
   )
+}
+
+// https://saurav.tech/NewsAPI/top-headlines/category/business/us.json
+
+export async function getServerSideProps(){
+  const newsResults = await fetch('https://saurav.tech/NewsAPI/top-headlines/category/business/us.json')
+  .then(res => res.json())
+
+  return {
+    props: {
+      newsResults
+    }
+  }
+
 }
